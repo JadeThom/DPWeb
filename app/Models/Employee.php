d40ca6812bb590abd6a39fc98e44334232747b92
JadeThom <jadethomaz83@gmail.com>
2021-11-10 21:16:13 -0500

    commit

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -8,4 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class Employee extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['photo',
+        'name',
+        'date_birth',
+        'sex',
+        'phone',
+        'civil_status',
+        'mother',
+        'father',
+        'nationality',
+        'naturalness',
+        'address_id',
+        'breed',
+        'deficient',
+        'blood'];
+
+    public function address(){
+        return $this->belongsTo(Address::class, 'address_id', 'id');
+    }
 }
--- database/migrations/2021_11_11_010955_create_employees_table.php
+++ database/migrations/2021_11_11_010955_create_employees_table.php
@@ -15,7 +15,23 @@ class CreateEmployeesTable extends Migration
     {
         Schema::create('employees', function (Blueprint $table) {
             $table->id();
+            $table->string('photo');
+            $table->string('name');
+            $table->date('date_birth');
+            $table->boolean('sex');
+            $table->string('phone');
+            $table->string('civil_status');
+            $table->string('mother');
+            $table->string('father');
+            $table->string('nationality');
+            $table->string('naturalness');
+            $table->unsignedBigInteger('address_id');
+            $table->string('breed');
+            $table->boolean('deficient');
+            $table->boolean('blood');
             $table->timestamps();
+
+            $table->foreign('address_id')->references('id')->on('addresses');
         });
     }
 
